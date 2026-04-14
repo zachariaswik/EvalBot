591a2341d03877e168f0a745980e822358b4b000
Zacharias Wik <erikzwik@gmail.com>
2026-04-14 11:04:54 +0200

    Remove "built with reflex"

--- rxconfig.py
+++ rxconfig.py
@@ -5,6 +5,7 @@ config = rx.Config(
     app_name="frontend",
     db_url="sqlite:///reflex.db",
     telemetry_enabled=False,
+    show_built_with_reflex=False,
     state_auto_setters=False,
     plugins=[SitemapPlugin()],
 )
